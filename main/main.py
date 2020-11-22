3d4f83ad74af9f9014eba45cc4ce8002fa4d8293
“jackhhh” <me@jackh.cn>
2020-11-23 03:50:54 +0800

    Integrate optimized or edited algorithm.

--- main/main.py
+++ main/main.py
@@ -17,18 +17,34 @@ if __name__ == '__main__':
     print '1. UserKNN        2. ItemKNN        3. BasicMF        4. SlopeOne        5. SVD'
     print '6. PMF            7. SVD++          8. EE             9. BPR             10. WRMF'
     print '11. ExpoMF'
+    print '-' * 80
+
+    print '***Optimized Generic Recommenders:***'
+    print '12. BPR-Optimizer'
+    print '-' * 80
 
     print 'Social Recommenders:'
     print 's1. RSTE          s2. SoRec         s3. SoReg         s4. SocialMF       s5. SBPR'
     print 's6. SREE          s7. LOCABAL       s8. SocialFD      s9. TBPR           s10. SERec'
+    print '-' * 80
 
     print 'Network Embedding based Recommenders:'
     print 'a1. CoFactor      a2. CUNE-MF       a3. CUNE-BPR      a4. IF-BPR'
+    print '-' * 80
+
+    print '***Optimized Network Embedding based Recommenders:***'
+    print 'a5. IF-BPR-Optimizer'
+    print '-' * 80
 
     print 'Deep Recommenders:'
     print 'd1. APR           d2. CDAE          d3. DMF           d4. NeuMF           d5. CFGAN'
     print 'd6. IRGAN         d7. RSGAN         d8. NGCF          d9. LightGCN        d10. ESRF'
     print 'd11. DHCF         d12. DiffNet'
+    print '-' * 80
+
+    print '***Optimized Deep Recommenders:***'
+    print 'd13. RSGAN Softmax 0.1              d14. RSGAN Minibatch'
+    print '-' * 80
 
     print 'Baselines:'
     print 'b1. UserMean      b2. ItemMean      b3. MostPopular   b4. Rand'
@@ -51,12 +67,12 @@ if __name__ == '__main__':
     #     exit(0)
 
     algorthms = {'1':'UserKNN','2':'ItemKNN','3':'BasicMF','4':'SlopeOne','5':'SVD','6':'PMF',
-                 '7':'SVD++','8':'EE','9':'BPR','10':'WRMF','11':'ExpoMF',
+                 '7':'SVD++','8':'EE','9':'BPR','10':'WRMF','11':'ExpoMF', '12':'BPR_Optimizer',
                  's1':'RSTE','s2':'SoRec','s3':'SoReg','s4':'SocialMF','s5':'SBPR','s6':'SREE',
                  's7':'LOCABAL','s8':'SocialFD','s9':'TBPR','s10':'SEREC','a1':'CoFactor',
-                 'a2':'CUNE_MF','a3':'CUNE_BPR','a4':'IF_BPR',
+                 'a2':'CUNE_MF','a3':'CUNE_BPR','a4':'IF_BPR', 'a5':'IF_BPR_Optimizer',
                  'd1':'APR','d2':'CDAE','d3':'DMF','d4':'NeuMF','d5':'CFGAN','d6':'IRGAN','d7':'RSGAN','d8':'NGCF',
-                 'd9':'LightGCN', 'd10':'ESRF', 'd11':'DHCF', 'd12':'DiffNet',
+                 'd9':'LightGCN', 'd10':'ESRF', 'd11':'DHCF', 'd12':'DiffNet', 'd13': 'RSGAN_Softmax0.1', 'd14': 'RSGAN_Minibatch',
                  'b1':'UserMean','b2':'ItemMean','b3':'MostPopular','b4':'Rand'}
 
     try:
